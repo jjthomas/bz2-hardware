7b8b4b56d55fbacbcf937dafebee57e1034a8245
James Thomas <jamesjoethomas@gmail.com>
2018-04-27 15:46:17 -0700

    buffer input and output in CUDA JSON

--- c-impls/json-field-extractor-generic.cu
+++ c-impls/json-field-extractor-generic.cu
@@ -20,6 +20,8 @@ using namespace std;
 #define NUM_BLOCKS (NUM_SMS * NUM_BLOCKS_PER_SM)
 #define NUM_THREADS (NUM_THREADS_PER_SM * NUM_SMS)
 
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
 typedef enum {EXP_KEY, IN_KEY, EXP_COL, EXP_VAL, IN_VAL, EXP_COM} state;
 
 typedef uint8_t uint1_t;
@@ -39,11 +41,11 @@ __global__ void run(uint8_t *input_full, uint8_t num_seq_confs, uint8_t num_spli
   uint8_t *output_full, uint32_t *output_count) {
 
   uint64_t index = blockIdx.x * blockDim.x + threadIdx.x;
-  uint8_t *input = input_full + index * input_count;
-  uint8_t *output = output_full + index * input_count;
+  uint8_t *input_buf = input_full + index * input_count;
+  uint8_t *output_buf = output_full + index * input_count;
 
   uint32_t input_idx = 0;
-  uint32_t output_idx = 0;
+  uint32_t output_buf_idx = 0;
   state parse_state = EXP_VAL;
   uint8_t match_state = 0; // caps number of chars in fields to match at ~254
   uint1_t in_string_value = 0;
@@ -66,106 +68,120 @@ __global__ void run(uint8_t *input_full, uint8_t num_seq_confs, uint8_t num_spli
 
   // match_states are exactly 8 bits only when MAX_FIELD_CHARS is >= 127
   for (uint8_t i = 0; i < num_seq_confs; i++) {
-    seq_trans[i] = (seq_entry){input[input_idx], input[input_idx + 1]};
+    seq_trans[i] = (seq_entry){input_buf[input_idx], input_buf[input_idx + 1]};
     input_idx += 2;
   }
   for (uint8_t i = 0; i < num_split_confs; i++) {
-    split_trans[i] = (split_entry){input[input_idx], input[input_idx + 1], input[input_idx + 2]};
+    split_trans[i] = (split_entry){input_buf[input_idx], input_buf[input_idx + 1], input_buf[input_idx + 2]};
     input_idx += 3;
   }
   for (uint8_t i = num_split_confs; i < MAX_FIELDS; i++) {
     split_trans[i] = (split_entry){0, 0, 0}; // need to zero-initialize since all entries checked below
   }
-  for (uint32_t i = input_idx; i < input_count; i++) {
-    #define EMIT_CUR_TOKEN do {\
-              if (match_state == MAX_FIELD_CHARS) {\
-                output[output_idx++] = input[i];\
-              }\
-            } while (0)
+  #define BUF_SIZE 64
+  uint8_t input[BUF_SIZE];
+  uint8_t output[BUF_SIZE]; // it is possible that output is larger than input, but with fairly large BUF_SIZE
+  // on the kafka_json dataset it shouldn't happen
+  for (uint32_t ii = input_idx; ii < input_count; ii += BUF_SIZE) {
+    for (uint32_t i = ii; i < MIN(input_count, ii + BUF_SIZE); i++) {
+      input[i - ii] = input_buf[i];
+    }
+    uint32_t output_idx = 0;
+    for (uint32_t i = 0; i < MIN(input_count - ii, BUF_SIZE); i++) {
+      #define EMIT_CUR_TOKEN do {\
+                if (match_state == MAX_FIELD_CHARS) {\
+                  output[output_idx++] = input[i];\
+                }\
+              } while (0)
 
-    // only need a next variable for parse_state because the other two state variables used in if conditions
-    // (in_string_value and nest_depth) are not at risk of an erroneous read after write
-    state next_parse_state = parse_state;
-    if (parse_state == EXP_VAL) {
-      if (input[i] == '{') {
-        next_parse_state = EXP_KEY;
-        nest_depth++;
-      } else if (nest_depth != 0 && !IS_WHITESPACE(input[i])) {
-        // at nest_depth of 0 we only accept new records
-        EMIT_CUR_TOKEN;
-        next_parse_state = IN_VAL;
-        in_string_value = input[i] == '"';
-      }
-    }
-    if (parse_state == IN_VAL) {
-      if (in_string_value) {
-        EMIT_CUR_TOKEN;
-        if (input[i] == '"' && last_char != '\\') {
-          in_string_value = 0;
-        }
-      } else if (input[i] != '}') {
-        if (input[i] == ',') {
+      // only need a next variable for parse_state because the other two state variables used in if conditions
+      // (in_string_value and nest_depth) are not at risk of an erroneous read after write
+      state next_parse_state = parse_state;
+      if (parse_state == EXP_VAL) {
+        if (input[i] == '{') {
           next_parse_state = EXP_KEY;
-          POP_STATE_STACK;
-        } else {
+          nest_depth++;
+        } else if (nest_depth != 0 && !IS_WHITESPACE(input[i])) {
+          // at nest_depth of 0 we only accept new records
           EMIT_CUR_TOKEN;
+          next_parse_state = IN_VAL;
+          in_string_value = input[i] == '"';
         }
       }
-    }
-    if (input[i] == ',' && parse_state == EXP_COM) {
-      next_parse_state = EXP_KEY;
-      POP_STATE_STACK;
-    }
-    if (input[i] == '}' &&
-      (parse_state == EXP_KEY || parse_state == EXP_COM || (parse_state == IN_VAL && !in_string_value))) {
-      if (parse_state == EXP_COM || parse_state == IN_VAL) {
-        POP_STATE_STACK;
-      }
-      if (nest_depth == 1) {
-        output[output_idx++] = '/'; // record separator
-        next_parse_state = EXP_VAL;
-      } else {
-        next_parse_state = EXP_COM;
-      }
-      nest_depth--;
-    }
-    if (input[i] == '"' && parse_state == IN_KEY) {
-      next_parse_state = EXP_COL;
-    }
-    if (input[i] == ':' && parse_state == EXP_COL) {
-      next_parse_state = EXP_VAL;
-    }
-    uint1_t entering_key = input[i] == '"' && parse_state == EXP_KEY;
-    if (entering_key) {
-      next_parse_state = IN_KEY;
-      state_stack[stack_ptr++] = match_state;
-    }
-    if ((parse_state == IN_KEY || entering_key) && match_state != MAX_FIELD_CHARS &&
-      (match_state != 0 || nest_depth == 1)) {
-      // only allow match to start at top level
-      if (input[i] == seq_trans[match_state].expected_input) {
-        if (seq_trans[match_state].next_match_state == MAX_FIELD_CHARS) {
-          output[output_idx++] = match_state;
-        }
-        match_state = seq_trans[match_state].next_match_state;
-      } else {
-        uint8_t next_match_state = 0;
-        for (uint8_t j = 0; j < MAX_FIELDS; j++) {
-          if (match_state == split_trans[j].expected_match_state && input[i] == split_trans[j].expected_input) {
-            if (split_trans[j].next_match_state == MAX_FIELD_CHARS) {
-              output[output_idx++] = match_state;
-            }
-            next_match_state = split_trans[j].next_match_state;
+      if (parse_state == IN_VAL) {
+        if (in_string_value) {
+          EMIT_CUR_TOKEN;
+          if (input[i] == '"' && last_char != '\\') {
+            in_string_value = 0;
+          }
+        } else if (input[i] != '}') {
+          if (input[i] == ',') {
+            next_parse_state = EXP_KEY;
+            POP_STATE_STACK;
+          } else {
+            EMIT_CUR_TOKEN;
           }
         }
-        match_state = next_match_state;
       }
-    }
+      if (input[i] == ',' && parse_state == EXP_COM) {
+        next_parse_state = EXP_KEY;
+        POP_STATE_STACK;
+      }
+      if (input[i] == '}' &&
+        (parse_state == EXP_KEY || parse_state == EXP_COM || (parse_state == IN_VAL && !in_string_value))) {
+        if (parse_state == EXP_COM || parse_state == IN_VAL) {
+          POP_STATE_STACK;
+        }
+        if (nest_depth == 1) {
+          output[output_idx++] = '/'; // record separator
+          next_parse_state = EXP_VAL;
+        } else {
+          next_parse_state = EXP_COM;
+        }
+        nest_depth--;
+      }
+      if (input[i] == '"' && parse_state == IN_KEY) {
+        next_parse_state = EXP_COL;
+      }
+      if (input[i] == ':' && parse_state == EXP_COL) {
+        next_parse_state = EXP_VAL;
+      }
+      uint1_t entering_key = input[i] == '"' && parse_state == EXP_KEY;
+      if (entering_key) {
+        next_parse_state = IN_KEY;
+        state_stack[stack_ptr++] = match_state;
+      }
+      if ((parse_state == IN_KEY || entering_key) && match_state != MAX_FIELD_CHARS &&
+        (match_state != 0 || nest_depth == 1)) {
+        // only allow match to start at top level
+        if (input[i] == seq_trans[match_state].expected_input) {
+          if (seq_trans[match_state].next_match_state == MAX_FIELD_CHARS) {
+            output[output_idx++] = match_state;
+          }
+          match_state = seq_trans[match_state].next_match_state;
+        } else {
+          uint8_t next_match_state = 0;
+          for (uint8_t j = 0; j < MAX_FIELDS; j++) {
+            if (match_state == split_trans[j].expected_match_state && input[i] == split_trans[j].expected_input) {
+              if (split_trans[j].next_match_state == MAX_FIELD_CHARS) {
+                output[output_idx++] = match_state;
+              }
+              next_match_state = split_trans[j].next_match_state;
+            }
+          }
+          match_state = next_match_state;
+        }
+      }
 
-    last_char = input[i];
-    parse_state = next_parse_state;
+      last_char = input[i];
+      parse_state = next_parse_state;
+    }
+    for (uint32_t i = 0; i < output_idx; i++) {
+      output_buf[output_buf_idx + i] = output[i];
+    }
+    output_buf_idx += output_idx;
   }
-  *(output_count + index) = output_idx;
+  *(output_count + index) = output_buf_idx;
 }
 
 int main(int argc, char **argv) {
